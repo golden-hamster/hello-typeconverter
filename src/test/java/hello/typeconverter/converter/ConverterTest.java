d21a56e8b2e1fef68fa32f4fd342540a02483ef9
golden-hamster <isack19999@gmail.com>
2023-04-13 17:31:25 +0900

    typeConverter

---
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,34 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void stringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        //@EqualsAndHashCode 덕분에 참조값이 달라도 들어있는 데이터만 같으면 true 가 나온다.
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8008));
+    }
+}
